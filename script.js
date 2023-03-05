2c9c0d2d6a7867c15a5b3b2fca5f18cc7f862536
Luiz Felipe <luizfelipesoma@gmail.com>
2023-03-04 23:04:33 -0300

    JS Ajeitado com fũncoes a mais

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-import { Pessoa } from "./Pessoa.js";
-
 const inNome = document.getElementById("inNome");
 const inIdade = document.getElementById("inIdade");
 const inCPF = document.getElementById("inCPF");
@@ -8,3 +6,73 @@ const formulario = document.getElementById("formulario");
 
 const btMostrar = document.getElementById("btMostrar");
 
+const outputTable = document.getElementById("outputTable");
+
+formulario.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const formData = new FormData(formulario);
+
+    fetch('./cadastrarPessoas.php', {
+        method: 'POST',
+        body: formData
+    })
+        .then(response => response.text())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+});
+
+btMostrar.addEventListener('click', function () {
+    let vetListaDePessoas = [];
+
+    fetch('obterPessoas.php', {
+        method: 'GET'
+    })
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(item => {
+                vetListaDePessoas.push([item.nome, item.idade, item.cpf]);
+            });
+        })
+        .catch(error => {
+            console.error(error);
+        });
+
+    recriarIndice();
+
+    for (let i = 0; i < vetListaDePessoas.length; i++) {
+        const tr = document.createElement("tr");
+        for (let j = 0; j < vetListaDePessoas[i]; j++) {
+            let td = document.createElement("td");
+            td.textContent = `${vetListaDePessoas[i][j]}`;
+            tr.appendChild(td);
+        }
+        outputTable.appendChild(tr);
+    }
+});
+
+function recriarIndice() {
+    const tr = document.createElement("tr");
+    for (var i = 0; i < 4; i++) {
+        const th = document.createElement("th");
+        switch (i) {
+            case 0:
+                th.textContent = "Código";
+                break;
+            case 1:
+                th.textContent = "Nome";
+                break;
+            case 2:
+                th.textContent = "Idade";
+                break;
+            case 3:
+                th.textContent = "CPF";
+        }
+        tr.appendChild(th);
+    }
+    outputTable.appendChild(tr);
+}
